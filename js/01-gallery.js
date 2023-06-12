83118ce2abc15ca26ce2d7c4aff42501ec21db37
LizaMakeeva <123816991+LizaMakeeva@users.noreply.github.com>
2023-06-12 11:49:37 +0300

    +

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,33 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+const galleryEl = document.querySelector(".gallery");
+const galleryAdd = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `<li class="gallery__item"><a class="gallery__link" href = "${original}" >
+    <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" /> </a >
+    </li>`
+  )
+  .join("");
+galleryEl.innerHTML = galleryAdd;
 
+galleryEl.addEventListener("click", onGalleryClick);
+
+function onGalleryClick(event) {
+  event.preventDefault();
+  if (event.target.nodeName !== "IMG") return;
+  const instance = basicLightbox.create(`
+    <img src="${event.target.dataset.source}" alt="${event.target.alt}" />
+  `);
+
+  instance.show();
+
+  window.addEventListener("keydown", onGalleryEsc);
+  function onGalleryEsc(event) {
+    if (event.code === "Escape") {
+      instance.close();
+      window.removeEventListener("keydown", onGalleryEsc);
+    }
+  }
+}
 console.log(galleryItems);
